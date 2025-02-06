0f72d293b3123b4772e939dcf4d5bd4f7716bc6f
msharafice <mohamad.sharafi.ce@gmail.com>
2025-02-06 14:18:57 +0330

    adding advanced keyword

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -13,12 +13,15 @@ class CalculatorScreenState extends State<CalculatorScreen> {
   String input = "";
   String result = "0";
   bool showAdvancedKeyboard = false;
+  bool showSecond = false;
+  bool isDegDisabled = false;
+  bool isDegreeMode = true; 
   List<String> history = [];
   bool showHistory = false;
 
   void buttonPressed(String value) {
     setState(() {
-      if (value == "C") {
+      if (value == "AC") {
         input = "";
         result = "0";
       } else if (value == "⌫") {
@@ -37,8 +40,12 @@ class CalculatorScreenState extends State<CalculatorScreen> {
               .replaceAllMapped(RegExp(r'cos\s*\(?([\d.]+)\)?'),
                   (match) => 'cos((${match[1]} * pi / 180))')
               .replaceAllMapped(RegExp(r'tan\s*\(?([\d.]+)\)?'),
-                  (match) => 'tan((${match[1]} * pi / 180))');
-
+                  (match) => 'tan((${match[1]} * pi / 180))')
+              .replaceAll("π", "pi")
+              .replaceAllMapped(
+                RegExp(r'√\(?([\d.]+)\)?'),
+                (match) => 'sqrt(${match[1]})',
+              );
           final parser = MathNodeExpression.fromString(formattedInput);
           final evaluated =
               parser.calc(MathVariableValues({'pi': 3.141592653589793}));
@@ -47,17 +54,53 @@ class CalculatorScreenState extends State<CalculatorScreen> {
               .replaceAll(RegExp(r"0*$"), "")
               .replaceAll(RegExp(r"\.$"), "");
           history.add("$input = $result");
+          input = "";
         } catch (e) {
           result = "خطا";
         }
       } else if (value == "2st") {
         showAdvancedKeyboard = !showAdvancedKeyboard;
+      } else if (value == "2nd") {
+        // تغییر حالت صفحه‌کلید پیشرفته
+        setState(() {
+          showSecond = !showSecond; // تغییر وضعیت 2nd
+        });
+      } else if (value == "√") {
+        input += "√(";
+      } else if (value == "⁻¹") {
+        setState(() {
+          if (input.isNotEmpty) {
+            input = "($input)^(-1)";
+          }
+        });
+      } else if (value == "x^y") {
+        // فقط ^ را به ورودی اضافه کن
+        setState(() {
+          if (input.isNotEmpty && !input.contains("^")) {
+            input += "^"; // به ورودی فقط ^ اضافه کن
+          }
+        });
+      } else if (value == "e") {
+        // نمایش عدد e تا 20 رقم اعشار
+        setState(() {
+          input += "e"; // نمایش عدد e تا 20 رقم اعشار
+          result = "2.71828182845904523536";
+        });
+      } else if (value == "π") {
+        setState(() {
+          input += "π";
+          result = "3.141592653589793238";
+        });
       } else {
         input += value;
       }
     });
   }
-
+  void toggleDegRad() {
+  setState(() {
+    isDegreeMode = !isDegreeMode;
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -110,9 +153,14 @@ class CalculatorScreenState extends State<CalculatorScreen> {
                   ),
                 ),
                 // advanced keyboard
-                if (showAdvancedKeyboard) buildAdvancedKeyboard(buttonPressed),
+                if (!showSecond && showAdvancedKeyboard)
+                  buildAdvancedKeyboard(buttonPressed),
+                // advanced 2nd keyboard
+                if (showSecond) buildSecondKeyboard(buttonPressed),
+                
                 // defult keyboard
-                if (!showAdvancedKeyboard) buildDefaultKeyboard(buttonPressed),
+                if (!showSecond && !showAdvancedKeyboard)
+                  buildDefaultKeyboard(buttonPressed),
               ],
             ),
     );
